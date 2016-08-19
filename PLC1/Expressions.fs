ed642cd50f4bb4579aa94543843fb21e20aef068
2483 <2483@LT-2440>
2016-08-19 13:53:49 +0200

    Add project files.

---
+++ PLC1/Expressions.fs
@@ -0,0 +1,17 @@
+﻿module expressions
+
+type expr =
+    | CstI of int
+    | Var of string
+    | Prim of string * expr * expr
+
+let rec eval (e : expr) : int =
+    match e with
+    | CstI i -> i
+    | Prim("+", e1, e2) -> eval e1 + eval e2
+    | Prim("*", e1, e2) -> eval e1 * eval e2
+    | Prim("-", e1, e2) -> eval e1 - eval e2
+    | Prim _            -> failwith "unknown primitive"
+
+
+
