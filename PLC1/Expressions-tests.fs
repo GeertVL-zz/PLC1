48671d0e7e493ecfa6debc9c249470d827acb2ea
Geert Van Laethem <geert.van.laethem@telenet.be>
2016-08-19 20:22:37 +0200

    Adding min and max

--- PLC1/Expressions-tests.fs
+++ PLC1/Expressions-tests.fs
@@ -1,4 +1,44 @@
 ﻿module expressionTests
 
+open NUnit.Framework
+open FsUnit
+open expressions
+
+let env = [("a", 3); ("c", 78); ("baf", 666); ("b", 111); ("x", 11)]
+let emptyenv = []
+
+[<Test>]
+let ``first test`` () =
+    1 |> should equal 1
+
+[<Test>]
+let ``Lookup an environment variable`` () =
+    lookup env "x" |> should equal 11
+
+[<Test>]
+let ``Evaluate a value`` () =
+    let ev = CstI 5
+    eval ev emptyenv |> should equal 5
+
+[<Test>]
+let ``Evaluate a variable`` () =
+    let ev = Var "x"
+    eval ev env |> should equal 11
+
+[<Test>]
+let ``Evaluate an addition`` () =
+    let ev = Prim("+", CstI 5, Var "a")
+    eval ev env |> should equal 8
+
+[<Test>]
+let ``Evaluate a subtraction`` () =
+    let ev = Prim("-", CstI 5, Var "a")
+    eval ev env |> should equal 2
+
+[<Test>]
+let ``Evaluate maximum of two numbers`` () =
+    let ev = Prim("max", CstI 5, Var "a")
+    eval ev env |> should equal 5
+
 
 
--- PLC1/Expressions.fs
+++ PLC1/Expressions.fs
@@ -5,13 +5,25 @@ type expr =
     | Var of string
     | Prim of string * expr * expr
 
-let rec eval (e : expr) : int =
+let rec lookup env x =
+    match env with
+    | []           -> failwith (x + " not found")
+    | (y, v)::r    -> if x=y then v else lookup r x 
+
+let rec eval (e : expr) (env: (string * int) list) : int =
     match e with
     | CstI i -> i
-    | Prim("+", e1, e2) -> eval e1 + eval e2
-    | Prim("*", e1, e2) -> eval e1 * eval e2
-    | Prim("-", e1, e2) -> eval e1 - eval e2
-    | Prim _            -> failwith "unknown primitive"
+    | Var x  -> lookup env x
+    | Prim(ope, e1, e2) -> 
+        let i1 = eval e1 env
+        let i2 = eval e2 env
+        match ope with
+        | "max" -> if i1 > i2 then i1 else i2
+        | "min" -> if i1 < i2 then i1 else i2
+        | "+"   -> eval e1 env + eval e2 env
+        | "-"   -> eval e1 env - eval e2 env 
+        | "*"   -> eval e1 env * eval e2 env
+        | _     -> failwith "operator does not exist"
 
 
 
